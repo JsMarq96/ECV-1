7298a6388022716e38a82112224fc7890a6363b2
Juan S. Marquerie <jsmarquerie@gmail.com>
2023-02-07 17:54:13 +0100

    Added basic world and instances, but tilebased animation is broken

--- room.js
+++ room.js
@@ -5,7 +5,7 @@ function get_image(url) {
   if (img_cache[url])
     return img_cache[url];
 
-  var img = imgs[url] = new Image();
+  var img = img_cache[url] = new Image();
   img.src = url;
   return img;
 }
@@ -16,30 +16,133 @@ var obj_template = {
   img: null,
   render: function(ctx, cam_scale) {
     const img = get_image(this.img);
-    ctx.drawImage(img, this.position.x, this.position.x, img.width * this.scale.x * cam_scale.x, img.height * this.scale.y * cam_scale.y);
+    ctx.drawImage(img,
+                  this.position.x,
+                  this.position.y,
+                  img.width * this.scale.x * cam_scale.x,
+                  img.height * this.scale.y * cam_scale.y);
   }
 };
+
+var user_template = {
+  position: {x: 0, y: 0},
+  speed: {x: 0, y:0},
+  facing_left: false,
+  scale: 1.0,
+  img: null,
+  tile_size_x: 0,
+  tile_size_y: 0,
+  tile_standby: 0,
+  tile_walk: [],
+
+  render: function(ctx, time, cam_scale) {
+    const img = get_image(this.img);
+    if (this.speed.x > 0.0 || this.speed.x < 0.0) {
+      // The character is moving
+      var frame = time % this.tile_walk.length;
+      var x_tile = (frame * this.tile_size_x) % img.width;
+      var y_tile = Math.floor(frame / (this.tile_size_x / img.width));
+    } else {
+      // Stopped character
+      var x_tile = (this.tile_standby * this.tile_size_x) % img.width;
+      var y_tile = Math.floor(this.tile_standby / (this.tile_size_x / img.width));
+    }
+    ctx.drawImage(img,
+                  x_tile, y_tile,
+                  this.tile_size_x, this.tile_size_y,
+                  this.position.x, this.position.y,
+                  this.tile_size_x * this.scale, this.tile_size_y * this.scale);
+  }
+};
+
 
 var World = {
   camera_pos: {x: 0, y: 0},
   camera_scale: {width: 0, height: 0},
   current_room: "",
   objects: {},
+  room_backgrounds: {},
+  last_time: performance.now(),
 
   render_frame: function() {
     var ctx = canvas.getContext('2d');
+
+    // Canvas scale & aspect ratio
+    var parent = canvas.parentNode;
+    var rect = parent.getBoundingClientRect();
+    canvas.width = rect.width;
+    canvas.height= rect.height;
+
+    ctx.imageSmoothingEnabled = false;
+    ctx.imageSmoothingQuality = "high";
 
     // Clear pass
     ctx.fillStyle = "#000000"; // Black
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // Set coordinate center on the center of teh scree, taking into accout the camera pos
+    ctx.translate(-World.camera_pos.x + canvas.width/ 2, -World.camera_pos.y + canvas.height / 2);
+
+    // Draw the background of the current room
+    World.room_backgrounds[World.current_room].render(ctx);
+
     // Render each object
-    for(var i = 0; i < World.objects.length(); i++) {
-      World.objects[this.current_room][i].render(ctx, {x: 1, y: 1});
+    for(var i = 0; i < World.objects[World.current_room].length; i++) {
+      World.objects[World.current_room][i].render(ctx, {x: 1, y: 1});
     }
+
+    var now = performance.now();
+    var elapsed_time = (now - World.last_time) / 1000;
+    last_time = now;
+
+    // Update
+
+    // Reset the camera transfomrs, return the axis to the origila pos, and send the animation frame
+    ctx.resetTransform();
+    requestAnimationFrame(World.render_frame);
+  },
+
+  create_room: function(name, image_url, scale) {
+    var new_room = {
+      img_url: image_url,
+      scale: scale,
+      render: function(ctx) {
+        var image = get_image(this.img_url);
+        var size = [image.width * scale, image.height * scale];
+        ctx.drawImage(image, -size[0] / 2, -size[1] / 2, size[0], size[1]);
+      }
+    };
+
+    this.room_backgrounds[name] = new_room;
+    this.objects[name] = [];
+  },
+
+  add_user_to_room: function (room_name, img, scale, tile_size_x, tile_size_y, tile_standby, tile_walk_anim) {
+    // Fill the user strucutre
+    var new_user = {...user_template};
+    new_user.scale = scale;
+    new_user.img = img;
+    new_user.tile_size_x = tile_size_x;
+    new_user.tile_size_y = tile_size_y;
+    new_user.tile_standby = tile_standby;
+    new_user.tile_walk_anim = tile_walk_anim;
+
+    this.objects[room_name].push(new_user);
+
+    // Returns the index of the current object
+    return this.objects[room_name].length - 1;
   }
 
 };
 
 
-draw();
+
+World.create_room("room_1", "imgs/mezeus-silent-hill.jpg", 0.86);
+var user_id = World.add_user_to_room("room_1",
+                                     "imgs/tileset.png",
+                                     4.0,
+                                     43, 43,
+                                     0,
+                                     [1, 2, 3, 4, 5, 6, 7]);
+World.current_room = "room_1";
+World.render_frame();
